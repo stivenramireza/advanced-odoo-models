65eaac80ed5e9b1fce792779a4f49fe3fccc01eb
Stiven Ramírez Arango <stivenramireza@gmail.com>
2021-04-22 18:28:47 -0500

    SQL Constraints

--- addons/odoo_model_advanced/models/models.py
+++ addons/odoo_model_advanced/models/models.py
@@ -3,10 +3,14 @@ from odoo import models, fields, api, exceptions
 
 class Car(models.Model):
     _name = 'odoo_model_advanced.car'
-    _description = 'Coche'
+    _description = 'Car'
 
-    name = fields.Char(string='Modelo')
-    number_plate = fields.Char(string='Matrícula')
+    name = fields.Char(string='Model')
+    license_plate = fields.Char(string='License plate')
     cv = fields.Float(string='CV')
-    colour = fields.Char(string='Color')
-    fuel_litres = fields.Float(string='Litros')
+    colour = fields.Char(string='Colour')
+    liters = fields.Float(string='Liters')
+
+    _sql_constraints = [
+        ('license_plate_unique', 'UNIQUE(license_plate)', 'License plate must be unique')
+    ]
